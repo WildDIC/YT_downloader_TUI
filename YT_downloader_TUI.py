0dd04fd8c3b92b8120023306ec0c5ccbb13e89c8
WildDIC <wilddic@gmail.com>
2023-02-02 14:38:57 +0300

    1. Данные по плейлистам, каналам, отдельным видео теперь хранятся в отдельном JSON-файле (YT_downloader.json)
    2. При скачивании видео сразу скачивается обложка видео (по возможности наилучшего качества)
    3. Пересмотрен алгоритм поиска максимально подходящего разрешения:
    	- сперва ищутся файлы, содержащие звук и видео
    	- скачивается видео с совпадающим разрешением или разрешением на 1 ступень выше
    	- если не найдено загружается отдельно видеопоток требуемого или максимального разрешения
    4. После скачивания и объединения видео\аудио в 1 файл скачивается обложка и добавляется в файл

--- YT_downloader_TUI.py
+++ YT_downloader_TUI.py
@@ -1,31 +1,20 @@
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-from pytube import YouTube
-from pytube import Playlist
-from pytube import Channel
+from pytube import YouTube, Playlist, Channel
 from pytube import exceptions as pytubeexceptions
 import ffmpeg
+import json
 import csv
 import os
-import sys
 import re
-from pathlib import PurePath, Path
+from pathlib import Path
 import subprocess
 import curses
+import requests
 from urllib.error import HTTPError
 
-
-ytdir = "i:\\YouTube\\"
-mylists = {
-    "LGR Hardware Videos": "PLFDCFCDFCF7E7ACDD",
-    "Уроки Arduino и программирования": "PLkW_wFQyyaEpfrCDA_F_e5vYeZ3WPCZWB",
-    "Уроки Arduino и программирования": "PLgAbBhxTglwmVxDDC5TSYUI91oZ0LZQMw",
-}
-
-mychannels = {
-    "arduinoLab": "@arduinoLab",
-    "Электротехника и электроника для программистов": "@Zefar91",
-}
+image = "e:\\Data\\cover.jpg"
 
 resolutions = [
     "4320p",
@@ -35,16 +24,18 @@ resolutions = [
     "720p",
     "480p",
     "360p",
-    "240p"
+    "240p",
+    "144p"
 ]
 
 '''
-Original idea taken from https://martechwithme.com/how-to-download-youtube-channel-videos-python/
+Original idea taken from
+https://martechwithme.com/how-to-download-youtube-channel-videos-python/
 Thanks!
 '''
 
 
-def safe_filename(s: str, max_length: int=255) -> str:
+def safe_filename(s: str, max_length: int = 255) -> str:
     """Sanitize a string making it safe to use as a filename.
 
     This function was based off the limitations outlined here:
@@ -83,16 +74,32 @@ def safe_filename(s: str, max_length: int=255) -> str:
     return filename[:max_length].rsplit(" ", 0)[0]
 
 
+def download_cover(videoId):
+    names = [
+        'maxresdefault',
+        'sddefault',
+        'hqdefault',
+        'mqdefault',
+        'default',
+    ]
+    for name in names:
+        tmburl = 'https://i.ytimg.com/vi/' + videoId + '/' + name + '.jpg'
+        response = requests.get(tmburl)
+        if response.headers['Content-Length'] != '1097':
+            break
+    open(image, "wb").write(response.content)
+
+
 def DownloadVideo(stdscr, video_link, folder, filename,
-                  maxres=None, adaptive=False):
+                  maxres=None, progressive=True):
     height, width = stdscr.getmaxyx()
 
-    logwin = curses.newwin(height-6, 22, 6, 0)
+    logwin = curses.newwin(height-6, 23, 6, 0)
     logwin.clear()
     logwin.border(' ', ' ', '_', ' ', '_', '_', ' ', ' ')
     logwin.refresh()
 
-    coderwin = curses.newwin(height-6, width-23, 6, 23)
+    coderwin = curses.newwin(height-6, width-24, 6, 24)
     coderwin.clear()
     coderwin.border(' ', ' ', '_', ' ', '_', '_', ' ', ' ')
     coderwin.refresh()
@@ -101,35 +108,54 @@ def DownloadVideo(stdscr, video_link, folder, filename,
     fill_line(stdscr, 5, 0, ' ', width, 2)
     stdscr.refresh()
 
-    logwin.addstr(1, 0, f"Video Started. {maxres}\n", curses.color_pair(2))
+    logwin.addstr(1, 0, "Video Started\n", curses.color_pair(2))
     logwin.refresh()
 
+    video = YouTube(video_link)
+
     if maxres is None:
-        video_file = YouTube(video_link).streams.\
+        video_file = video.streams.\
             order_by('resolution').desc().first().download()
     else:
         try:
-            video_file = YouTube(video_link).streams.\
-                filter(adaptive=adaptive, progressive=True, res=maxres).\
-                order_by('resolution').desc().first().download()
-        except AttributeError as err:
-            maxres = resolutions[resolutions.index(maxres) + 1]
-            DownloadVideo(stdscr, video_link, folder, filename,
-                          maxres=maxres, adaptive=True)
-            return None
-        except KeyError as err:
-            maxres = resolutions[resolutions.index(maxres) + 1]
-            DownloadVideo(stdscr, video_link, folder, filename,
-                          maxres=maxres, adaptive=True)
-            return None
+            print(video_link)
+            streams = video.streams.order_by('resolution').\
+                filter(progressive=progressive).desc()
+            res = dict((id, s.resolution) for id, s in enumerate(streams))
+            print(res)
+            if maxres in res.values():
+                key = [i for i, v in res.items() if v == maxres][0]
+            else:
+                resid = resolutions.index(maxres)
+                if resolutions.index(res[0]) > resid:
+                    if progressive:
+                        DownloadVideo(stdscr, video_link, folder, filename,
+                                      maxres=maxres, progressive=False)
+                        return None
+                    else:
+                        key = 0
+                else:
+                    key = resolutions.index(res[0])
+                    for i, v in res.items():
+                        if resolutions.index(v) == resid - 1:
+                            key = i
+                            break
+
+            logwin.addstr(f"Download Start {res[key]}\n", curses.color_pair(2))
+            logwin.refresh()
+
+            video_file = streams[key].download()
         except HTTPError as err:
-            coderwin.addstr(2, 0, err, curses.color_pair(5))
-            coderwin.refresh()
+            print(err)
             DownloadVideo(stdscr, video_link, folder, filename, maxres=maxres)
             return None
-    logwin.addstr("Video Done\n", curses.color_pair(2))
+        except TimeoutError as err:
+            print(err)
+            DownloadVideo(stdscr, video_link, folder, filename, maxres=maxres)
+            return None
+    logwin.addstr("Download Done\n", curses.color_pair(2))
+    logwin.refresh()
 
-    source_video = ffmpeg.input(video_file)
     vfdata = ffmpeg.probe(video_file)
     vdur = round(float(vfdata['format']['duration']))
     hours = str(vdur // 3600).zfill(2)
@@ -139,12 +165,14 @@ def DownloadVideo(stdscr, video_link, folder, filename,
     fill_line(stdscr, 4, 0, duration, width, 2)
     stdscr.refresh()
 
+    logwin.addstr("Video Done\n", curses.color_pair(2))
+    logwin.refresh()
+
     hasaudio = False
     for s in vfdata['streams']:
         if s['codec_type'] == 'audio':
             if s['codec_name'] == 'aac':
                 hasaudio = True
-                source_audio = ffmpeg.input(video_file)
                 audio_file = video_file
                 logwin.addstr("Audio Presents\n", curses.color_pair(2))
                 logwin.refresh()
@@ -152,23 +180,21 @@ def DownloadVideo(stdscr, video_link, folder, filename,
     if not hasaudio:
         logwin.addstr("Audio Started\n", curses.color_pair(2))
         logwin.refresh()
-        audio_file = YouTube(video_link).streams.filter(only_audio=True).\
+        audio_file = video.streams.filter(only_audio=True).\
             order_by('abr').desc().first().download(filename_prefix="audio_")
         logwin.addstr("Audio Done\n", curses.color_pair(2))
-        source_audio = ffmpeg.input(audio_file)
 
     logwin.addstr("Concatenation Started\n", curses.color_pair(2))
     logwin.refresh()
 
     cmd = f'ffmpeg -i "{video_file}" -i "{audio_file}" \
         -filter_complex [0][1]concat=a=1:n=1:v=1[s0] -map [s0] \
-        "{folder}/{filename}"'
+        "_{filename}"'
 
     reg = re.compile(r'(\d\d:\d\d:\d\d).*')
 
     with subprocess.Popen(cmd,
                           stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                          # bufsize=1, universal_newlines=True) as process:
                           bufsize=1,
                           encoding='cp1251', errors='ignore') as process:
         for line in process.stderr:
@@ -188,8 +214,29 @@ def DownloadVideo(stdscr, video_link, folder, filename,
                     fill_line(stdscr, 5, 0, curdurstr, width, 2)
                     stdscr.refresh()
 
-    logwin.addstr(4, 0, "Concatenation Done\n", curses.color_pair(2))
+    logwin.addstr("Concatenation Done\n", curses.color_pair(2))
+
+    logwin.addstr("Add Cover\n", curses.color_pair(2))
     logwin.refresh()
+
+    download_cover(video.video_id)
+    cmd = f'ffmpeg -i "_{filename}" -i "{image}" \
+            -map 0:0 -map 0:1 -c copy \
+            -map 1 -c:v:1 mjpeg -disposition:v:1 attached_pic \
+            "{folder}/{filename}"'
+
+    with subprocess.Popen(cmd,
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                          bufsize=1,
+                          encoding='cp1251', errors='ignore') as process:
+        for line in process.stderr:
+            if 'frame=' in line:
+                coderwin.addstr(1, 0, line, curses.color_pair(2))
+                coderwin.refresh()
+
+    logwin.addstr("Cover added\n", curses.color_pair(2))
+    logwin.refresh()
+    os.unlink('_' + filename)
     os.unlink(video_file)
     if not hasaudio:
         os.unlink(audio_file)
@@ -213,10 +260,10 @@ def DownloadList(stdscr, list_videos, folder, maxres=None):
             list_videos_downloaded.append(row[0])
 
     for video in list_videos:
+        video_count += 1
+
         fill_line(stdscr, 2, 0, video, width, 1)
         stdscr.refresh()
-
-        video_count = video_count + 1
 
         if video in list_videos_downloaded:
             cntstr = f'Video {video_count}/{total_video} already downloaded'
@@ -235,7 +282,6 @@ def DownloadList(stdscr, list_videos, folder, maxres=None):
 
             video_name = safe_filename(video_title) + ".mp4"
 
-            pathstr = Path(folder, video_name)
             fill_line(stdscr, 3, 0, video_name, width, 2)
             stdscr.refresh()
 
@@ -278,42 +324,62 @@ def DownloadChannel(stdscr, channel_link, folder, maxres=None):
 
 def fill_line(stdscr, line, col, string, width, pair):
     stdscr.addstr(line, col, string, curses.color_pair(pair))
-    stdscr.addstr(line, len(string) + col, " " * (width - len(string) - col),
-                  curses.color_pair(pair))
+    if len(string) < width:
+        stdscr.addstr(line,
+                      len(string) + col,
+                      " " * (width - len(string) - col),
+                      curses.color_pair(pair))
+
+
+def get_maxres(folder, defaultres):
+    res = [k for k, v in defaultres.items() if folder in v]
+    if len(res) == 1:
+        return res[0]
+    return "720p"
 
 
 def main(stdscr):
-
     stdscr.clear()
     stdscr.refresh()
 
     # Start colors in curses
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_WHITE)
-    curses.init_pair(5, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_WHITE,  curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_BLACK,  curses.COLOR_WHITE)
+    curses.init_pair(4, curses.COLOR_GREEN,  curses.COLOR_WHITE)
+    curses.init_pair(5, curses.COLOR_RED,    curses.COLOR_BLACK)
+
+    height, width = stdscr.getmaxyx()
 
     if not os.path.exists('youtube_export_history.csv'):
         open('youtube_export_history.csv', 'w').close()
 
-    total_folders = len(mylists)
+    with open('YT_downloader.json', encoding="utf8") as file:
+        data = json.load(file)
 
-    for folder_count, folder in enumerate(mylists):
-        height, width = stdscr.getmaxyx()
+        ytdir = data['ytdir']
+        mylists = data['mylists']
+        mychannels = data['mychannels']
+        myvideos = data['myvideos']
+        defaultres = data['defaultres']
+
+    total_folders = len(mylists)
+    folder_count = 0
+
+    for plid in mylists:
+        folder_count += 1
+        folder = mylists[plid]
+
         stdscr.clear()
 
         dirpath = Path(ytdir+folder)
-
         strdirpath = str(dirpath)
         fill_line(stdscr, 0, 0, strdirpath, width, 3)
         strfcnt = f'{folder_count}/{total_folders}'
         stdscr.addstr(0, width-len(strfcnt), strfcnt, curses.color_pair(3))
 
-        maxres = "720p"
-        if folder == "LGR Hardware Videos":
-            maxres = "480p"
+        maxres = get_maxres(folder, defaultres)
 
         if Path(dirpath).exists():
             csv_file = Path(dirpath, 'youtube_export_history.csv')
@@ -322,19 +388,60 @@ def main(stdscr):
             DownloadPlaylist(
                 stdscr=stdscr,
                 palylist_link="https://www.youtube.com/playlist?list="
-                + mylists[folder],
+                + plid,
                 folder=dirpath,
                 maxres=maxres)
 
-    # for folder in mychannels:
-    #     dirpath = Path(ytdir+folder)
-    #     if Path(dirpath).exists():
-    #         csv_file = Path(dirpath, 'youtube_export_history.csv')
-    #         if not os.path.exists(csv_file):
-    #             open(csv_file, 'w').close()
-    #         DownloadChannel(
-    #             channel_link="https://www.youtube.com/" + mychannels[folder],
-    #             folder=dirpath,
-    #             maxres="480p")
+    total_folders = len(mychannels)
+    folder_count = 0
+
+    for folder in mychannels:
+        folder_count += 1
+
+        dirpath = Path(ytdir+folder)
+        strdirpath = str(dirpath)
+        fill_line(stdscr, 0, 0, strdirpath, width, 3)
+        strfcnt = f'{folder_count}/{total_folders}'
+        stdscr.addstr(0, width-len(strfcnt), strfcnt, curses.color_pair(3))
+
+        maxres = get_maxres(folder, defaultres)
+
+        if Path(dirpath).exists():
+            csv_file = Path(dirpath, 'youtube_export_history.csv')
+            if not os.path.exists(csv_file):
+                open(csv_file, 'w').close()
+            DownloadChannel(
+                stdscr=stdscr,
+                channel_link="https://www.youtube.com/" + mychannels[folder],
+                folder=dirpath,
+                maxres=maxres)
+
+    total_folders = len(myvideos)
+    folder_count = 0
+
+    for folder in myvideos:
+        folder_count += 1
+
+        dirpath = Path(ytdir+folder)
+        strdirpath = str(dirpath)
+        fill_line(stdscr, 0, 0, strdirpath, width, 3)
+        strfcnt = f'{folder_count}/{total_folders}'
+        stdscr.addstr(0, width-len(strfcnt), strfcnt, curses.color_pair(3))
+
+        maxres = get_maxres(folder, defaultres)
+
+        if Path(dirpath).exists():
+            csv_file = Path(dirpath, 'youtube_export_history.csv')
+            if not os.path.exists(csv_file):
+                open(csv_file, 'w').close()
+
+            video_urls = list(map(lambda x: "https://www.youtube.com/watch?v="
+                                            + x, myvideos[folder]))
+            DownloadList(
+                stdscr=stdscr,
+                list_videos=video_urls,
+                folder=dirpath,
+                maxres=maxres)
+
 
 curses.wrapper(main)
